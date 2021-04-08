23eef6bc7eefb0a6d3518aa7e9de756e07f65301
chronald711 <ronnieross711@gmail.com>
2021-04-08 18:06:51 -0500

    playing with javascript and modals in html and css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+// Grab the h1 and first p tags and the play button
+// 
+
+const playButton = document.getElementById('play')
+// console.log(playButton)
+const restartButton = document.getElementById('restart')
+
+
+function clickPlay(e) {
+    playButton.onclick = alert('The game has begun')
+}
+
+playButton.addEventListener('click', clickPlay)
+
+function clickRestart(e) {
+    restartButton.onclick = alert('Start Over!')
+}
+restartButton.addEventListener('click', clickRestart)
