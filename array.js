fb24990c3132bb9c82ada85054bc6d87f6fe6cd4
Ronnie Ross <62971289+ronnieross711@users.noreply.github.com>
2021-04-12 14:53:28 -0500

    restructured with Jeremy

---
+++ array.js
@@ -0,0 +1,185 @@
+let questions = [
+    { question: 'What year Phish officially become a band',
+    choices: [1984, 2001, 1981, 1994],
+    answer: 1984
+},
+
+    
+{ question: "What is the name of Jimmy's cat",
+  choices: ["Sara", "Col. Forbin", "Poster NutBag", "Ocelot"],
+  answer: "Poster NutBag"
+},
+
+{ question: "How many Festivals has Phish thrown",
+  choices: [5, 7, 10, 11],
+  answer: 11
+
+},
+
+{ question: "Who is the Guitar maker of Trey Anastasio's guitar",
+  choices: ["Fender", "Gibson", "Modulus", 'Languedoc'],
+  answer: "Languedoc"
+},
+]
+    
+
+
+    
+
+
+
+ 
+
+
+
+
+ 
+What is the Drummer's name
+    Derek Trucks
+    Zach Rice
+    Neil Pert
+    Jon Fishman
+    Butto
+
+
+
+ 
+Who was the founding memeber of Phish originally, but left the band
+    Jeff Holdsworth
+    Page McConell
+    Jerry Garcia
+    Neil Young
+    Butto
+
+
+
+ 
+Which festival was thrown in 2003
+    Festival 8
+    IT
+    The Great Went
+    Clifford Ball
+    Butto
+
+
+
+ 
+What album hey cover Halloween 1996
+    Dark of The Moon
+    Remain In Light
+    Led Zeppelin IV
+    American Beauty
+    Butto
+
+
+
+ 
+What years hey tour Japan
+    1999
+    2000
+    2001
+    1998
+    Butto
+
+
+
+ 
+What year he NYE gag go wrong, and Trey got stuck
+    2019-2020
+    2018-2019
+    2015-2016
+    2020-2021
+    Butto
+
+
+
+ 
+Who was the first Phish Phan
+    Jimmy
+    Ronnie
+    Amy
+    Brad
+    Butto
+
+
+
+ 
+Finish the Lyric I saw you ....
+    standing there
+    lookin' through the window
+    dancing in the street
+    with a ticket stub in your hand
+    Butto
+
+
+
+ 
+What song do they sing part of in Japanese
+    Stash
+    Gumbo
+    Meatstick
+    Golden Age
+
+
+
+
+ 
+Which years hish in on a hotdog for NYE
+    1996-1997 & 2000-2001
+    1999-2000 & 2010-2011
+    1989-1990 & 2011-2012
+    1999-2000 & 2011-2012
+
+
+
+
+ 
+Who wrote My Sweet One
+    Trey Anastasio
+    Page McConnell
+    Jon Fishman
+    Mike Gordon
+    Butto
+
+
+
+ 
+What islands hish go to for the legendary island tour
+    Bahamas
+    Fl Keys
+    Long Island & Rhode Island
+    Tr  & Tobago
+    Butto
+
+
+
+ 
+What festival was a mud pit
+    The Great Went
+    Coventry
+    Curveball
+    Magnaball
+    Butto
+
+
+
+ 
+Which band memeber graduated from the University of Vermont
+    Mike Gordon
+    Page McConnell
+    Jon Fishman
+    Trey Anastasio
+    Butto
+
+
+
+ 
+What is Page McConnell's nickname
+    Lawn Boy
+    Big Red
+    Leo
+    Piano Man
+    Butto
+
+
+
--- scripts.js
+++ scripts.js
@@ -4,32 +4,73 @@
 const playButton = document.getElementById('play')
 // console.log(playButton)
 const restartButton = document.getElementById('restart')
-const questions = document.querySelectorAll('.container')
-console.log(questions);
-// const modal1 = document.getElementById("modal-text-box")
-function togglePlay() {
-    let play = document.getElementById('play')
-    let displaySetting = play.style.display
-    if(displaySetting == 'block') {
-        play.style.display = 'none';
-        playButton.innerHTML = 'Show button';
-    } else {
-        play.style.display = 'block';
-        playButton.innerHTML = 'Hide button';
-    }
-} play.onlcick = togglePlay()
-// function disappear() {
-//    let  allQuestions = document.getElementsByClassName('answers');
-//    allQuestions.style.display = 'none'
-//    console.log(disappear())
-//    return allQuestions
-// }
-// disappear()
+const allQuestions = document.querySelectorAll('.container')
+// console.log(allQuestions);
+const main = document.querySelector('#container')
+
+let allQuestions = [
+    {question: "jkasdhfkjahsdf", answer}
+]
+
+let questions = allQuestions[0]
+// function userClick(){
+    // turning allQuestions into a proper array
+    let a = Array.from(allQuestions)
+    console.log(a)
+    let filteredList = a.filter(question => question.length)
+console.log(filteredList)
+
+function newFunction(x) {
+   return x.classList.contains('current')
+}
+
 function clickPlay(e) {
-    playButton.onclick = alert('The game has begun')
+    allQuestions[0].classList.add('current')
 }
 
 playButton.addEventListener('click', clickPlay)
+
+const nextButton = document.querySelector('#nextButton')
+const q1= document.getElementById('#question1')
+const q2 = document.getElementById('#question2')
+const q3 = document.getElementById('#question3')
+
+function startGame() {
+    playButton.classList.add('hide')
+}
+function goNext(e) {
+      allQuestions[1].classList.add('current')
+}
+nextButton.addEventListener('click', goNext)
+
+
+// First Question
+const answer1 = document.querySelector("#answer1")
+function correctAnswer(e) {
+    answer1.onclick = alert('Correct!')
+}
+answer1.addEventListener('click', correctAnswer)
+
+const answer2 = document.querySelector('#answer2')
+function wrongAnswer(e) {
+    answer2.onclick = alert('Wrong, try again')
+}
+answer2.addEventListener('click', wrongAnswer)
+
+const answer3 = document.querySelector('#answer3')
+function wrongAnswer(e) {
+    answer3.onlcick = alert('Wrong, try again')
+}
+answer3.addEventListener('click', wrongAnswer)
+
+const answer4 = document.querySelector('#answer4')
+function wrongAnswer(e) {
+    answer4.onlcick = alert('Wrong, try again')
+}
+answer4.addEventListener('click', wrongAnswer)
+// First Question
+
+
 
 function clickRestart(e) {
     restartButton.onclick = alert('Start Over!')
@@ -39,4 +80,3 @@ restartButton.addEventListener('click', clickRestart)
 // Do i need to make a function for each question to show on the page?
 // Or should I just give them all the same id to make it easier?
 // Is the only way to do it by adding the code in the head of your html?
-// what is going on with my github?
